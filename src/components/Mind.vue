a3013edd42347ca70fe601a945fad394c2be9d42
Magomed Makhmudov <mmriadovich@gmail.com>
2020-07-17 16:57:10 +0300

    feat: mind

--- src/components/Mind.vue
+++ src/components/Mind.vue
@@ -1,21 +1,23 @@
 <template>
-    <div class="mind" style="margin-bottom:20px">
-        <span>
-        <figure class="avatar avatar-lg">
-            <img src="https://icons-for-free.com/iconfiles/png/512/basic+white-galaxy-131994931874908144.png" />
-        </figure>
-        </span>
-        <span style="margin-left:10px">
-        <b></b>
-        </span>
-        <blockquote>
-        <p>{{item.id}}</p>
-        </blockquote>
-    </div>
+  <div class="mind" style="margin-bottom:20px">
+    <span>
+      <figure class="avatar avatar-lg">
+        <img
+          src="https://icons-for-free.com/iconfiles/png/512/basic+white-galaxy-131994931874908144.png"
+        />
+      </figure>
+    </span>
+    <span style="margin-left:10px">
+      <b></b>
+    </span>
+    <blockquote>
+      <p>{{item.body}}</p>
+    </blockquote>
+  </div>
 </template>
 
 <script>
 export default {
-    props: ['item']
-}
+  props: ["item"]
+};
 </script>
